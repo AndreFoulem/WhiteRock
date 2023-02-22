a1e2779f382894f01327cbe2f4696ab76604b3f7
AndreFoulem <timmycasual@icloud.com>
2023-02-21 16:37:45 -0800

    Initial Commit

---
+++ WhiteRock/WhiteRockApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteRockApp.swift
+//  WhiteRock
+//
+//  Created by AndreMacBook on 2023-02-21.
+//
+
+import SwiftUI
+
+@main
+struct WhiteRockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
